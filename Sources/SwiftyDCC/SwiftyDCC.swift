4e65d3d24d5594efe4ba1932c6a55100a14e5fbf
Jimmy Hough <jimmyhoughjr@me.com>
2022-12-18 05:53:00 -0600

    Initial commit, seems the tools are working now

---
+++ Sources/SwiftyDCC/SwiftyDCC.swift
@@ -0,0 +1,8 @@
+@main
+public struct SwiftyDCC {
+    public private(set) var text = "Hello, World!"
+
+    public static func main() {
+        print(SwiftyDCC().text)
+    }
+}
---
+++ Tests/SwiftyDCCTests/SwiftyDCCTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftyDCC
+
+final class SwiftyDCCTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftyDCC().text, "Hello, World!")
+    }
+}
